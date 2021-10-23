29a9bde98448fc09af6e152d34a0820e9b60d65e
MarselBurdo <9003363@gmail.com>
2021-10-23 11:51:49 +0300

    styles

--- src/Components/Wrapper/Wrapper.jsx
+++ src/Components/Wrapper/Wrapper.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import Sidebar from "../Sidebar/Sidebar";
 import Main from "../Main/Main";
 import { Container } from "./Wrapper.styles";
+import Task from "../Tasks/Task/Task";
+
 const Wrapper = () => {
   return (
     <Container>
       <Sidebar />
-      <Main />
+      {/* <Main /> */}
+
+      <Task />
     </Container>
   );
 };
--- src/Components/Wrapper/Wrapper.styles.jsx
+++ src/Components/Wrapper/Wrapper.styles.jsx
@@ -4,4 +4,5 @@ export const Container = styled.div`
   background-color: #f5f5f5;
   display: grid;
   grid-template-columns: 320px auto;
+  height: 100%;
 `;
