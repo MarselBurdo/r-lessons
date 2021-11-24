8c55121b8ad218c6ada67c195e0c95da15fb82ea
MarselBurdo <9003363@gmail.com>
2021-11-24 22:00:12 +0300

    redux

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -1,5 +1,5 @@
 export const GET_NOTES = "GET_NOTES";
 
-export const COUNT_INCREMENT = "increment Counter";
+export const COUNT_INCREMENT = "increment counter";
 export const COUNT_MULTIPLY = "multiply NUM";
 export const COUNT_CONCAT = "concat text";
